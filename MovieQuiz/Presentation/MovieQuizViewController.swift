2cb78236d3ce2cf7bf03288df278ff9b07a0bd74
LongCat17 <lein79@mail.ru>
2023-05-05 18:34:03 +0800

    Все переменные и методы переставлены согласно правилу, в viewDidLoad добавлен вызов метода show для вывода на экран элементов при запуске приложения, в метод yes/noButtonClicked добавлен код для проверки правильности ответа

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -2,87 +2,98 @@ import UIKit
 
 final class MovieQuizViewController: UIViewController {
     // MARK: - Lifecycle
+    
+// переменные
+    // текущий вопрос
+    private var currentQuestionIndex = 0
+    // правильные ответы
+    private var correctAnswers = 0
+    
+// массив вопросов
+    private let questions: [QuizQuestion] = [
+        QuizQuestion(
+            image: "The Godfather",
+            text: "Рейтинг этого фильма больше, чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Dark Knight",
+            text: "Рейтинг этого фильма больше, чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Kill Bill",
+            text: "Рейтинг этого фильма больше, чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Avengers",
+            text: "Рейтинг этого фильма больше, чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Deadpool",
+            text: "Рейтинг этого фильма больше, чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Green Knight",
+            text: "Рейтинг этого фильма больше, чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Old",
+            text: "Рейтинг этого фильма больше, чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "The Ice Age Adventures of Buck Wild",
+            text: "Рейтинг этого фильма больше, чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Tesla",
+            text: "Рейтинг этого фильма больше, чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Vivarium",
+            text: "Рейтинг этого фильма больше, чем 6?",
+            correctAnswer: false)
+    ]
+    
+    // модель результат квиза
+        struct QuizResultsViewModel {
+            let title: String
+            let text: String
+            let buttonText: String
+        }
+    // структура вопроса
+        struct QuizQuestion {
+            let image: String
+            let text: String
+            let correctAnswer: Bool
+        }
+    
+// DID LOAD
     override func viewDidLoad() {
         super.viewDidLoad()
+        show(quiz: QuizStepViewModel(image: UIImage(named: "The Godfather")!, question: "Рейтинг этого фильма больше, чем 6?", questionNumber: "\(currentQuestionIndex)/10"))
     }
-    // функции кнопок и интерактивных элементов
+// функции кнопок и интерактивных элементов
     @IBAction private func yesButtonClicked(_ sender: UIButton) {
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = true
         
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
     @IBAction private func noButtonClicked(_ sender: UIButton) {
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = false
         
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
     @IBOutlet private var imageView: UIImageView!
     @IBOutlet private var textLabel: UILabel!
     @IBOutlet private var counterLabel: UILabel!
     
-    // модель вопрос показан
+// модель вопрос показан
     struct QuizStepViewModel {
         let image: UIImage
         let question: String
         let questionNumber: String
     }
-    
-    // модель результат квиза
-    struct QuizResultsViewModel {
-        let title: String
-        let text: String
-        let buttonText: String
-    }
-    // структура вопроса
-    struct QuizQuestion {
-        let image: String
-        let text: String
-        let correctAnswer: Bool
-    }
-    // массив вопросов
-    private let questions: [QuizQuestion] = [
-        QuizQuestion(
-            image: "The Godfather",
-            text: "Рейтинг это фильма больше, чем 6?",
-            correctAnswer: true),
-        QuizQuestion(
-            image: "The Dark Knight",
-            text: "Рейтинг это фильма больше, чем 6?",
-            correctAnswer: true),
-        QuizQuestion(
-            image: "Kill Bill",
-            text: "Рейтинг это фильма больше, чем 6?",
-            correctAnswer: true),
-        QuizQuestion(
-            image: "The Avengers",
-            text: "Рейтинг это фильма больше, чем 6?",
-            correctAnswer: true),
-        QuizQuestion(
-            image: "Deadpool",
-            text: "Рейтинг это фильма больше, чем 6?",
-            correctAnswer: true),
-        QuizQuestion(
-            image: "The Green Knight",
-            text: "Рейтинг это фильма больше, чем 6?",
-            correctAnswer: true),
-        QuizQuestion(
-            image: "Old",
-            text: "Рейтинг это фильма больше, чем 6?",
-            correctAnswer: false),
-        QuizQuestion(
-            image: "The Ice Age Adventures of Buck Wild",
-            text: "Рейтинг это фильма больше, чем 6?",
-            correctAnswer: false),
-        QuizQuestion(
-            image: "Tesla",
-            text: "Рейтинг это фильма больше, чем 6?",
-            correctAnswer: false),
-        QuizQuestion(
-            image: "Vivarium",
-            text: "Рейтинг это фильма больше, чем 6?",
-            correctAnswer: false)
-    ]
-    // переменные
-    // текущий вопрос
-    private var currentQuestionIndex = 0
-    // правильные ответы
-    private var correctAnswers = 0
     
     // метод конвертации
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
